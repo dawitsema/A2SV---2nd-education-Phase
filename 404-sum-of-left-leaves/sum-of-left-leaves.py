ef1d7e0969dbb6b378c8e359cc3906bac1c04eba
dawitsema <128223243+dawitsema@users.noreply.github.com>
2024-04-14 10:10:53 +0300

    Time: 44 ms (7.59%) | Memory: 16.8 MB (47.79%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -6,25 +6,25 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        queue = deque([(root, False)])
-        ans = 0
-        while queue:
-            node, isLeft = queue.popleft()
-            if not node.left and not node.right and isLeft:
-                ans += node.val
-            if node.left:
-                queue.append((node.left, True))
-            if node.right:
-                queue.append((node.right, False))
-
-        return ans 
-        # def dfs(node, isLeft):
-        #     if not node: return 0
+        # queue = deque([(root, False)])
+        # ans = 0
+        # while queue:
+        #     node, isLeft = queue.popleft()
         #     if not node.left and not node.right and isLeft:
-        #         return node.val
-        #     return dfs(node.left, True) + dfs(node.right, False)
+        #         ans += node.val
+        #     if node.left:
+        #         queue.append((node.left, True))
+        #     if node.right:
+        #         queue.append((node.right, False))
+
+        # return ans 
+        def dfs(node, isLeft):
+            if not node: return 0
+            if not node.left and not node.right and isLeft:
+                return node.val
+            return dfs(node.left, True) + dfs(node.right, False)
         
-        # return dfs(root, False)
+        return dfs(root, False)
 
         
         
