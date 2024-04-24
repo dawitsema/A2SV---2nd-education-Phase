f41d940bc0749fef3ffc435e16b09dad4275f1c3
dawitsema <128223243+dawitsema@users.noreply.github.com>
2024-04-24 08:20:30 +0300

    Time: 29 ms (87.14%) | Memory: 16.5 MB (85.24%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,7 +1,8 @@
 class Solution:
-    def tribonacci(self, n: int) -> int:
-        nums = [0, 1, 1]
+    def __init__(self):
+        self.nums = [0, 1, 1]
         for i in range(35):
-            nums.append(nums[-1] + nums[-2] + nums[-3])
+            self.nums.append(self.nums[-1] + self.nums[-2] + self.nums[-3])
 
-        return nums[n]
+    def tribonacci(self, n: int) -> int:
+        return self.nums[n]
