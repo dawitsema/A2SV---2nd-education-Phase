ca6eef10eb183b425ecfbbb0968b4f8a477b2d0a
dawitsema <128223243+dawitsema@users.noreply.github.com>
2024-04-25 07:55:29 +0300

    Time: 1561 ms (37.78%) | Memory: 17.3 MB (93.33%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
@@ -4,7 +4,6 @@ class Solution:
         letters = [0] * 26
 
         res = 0
-        # Updating letters with the i-th character
         for i in range(N):
             curr = ord(s[i]) - ord('a')
             best = 0
@@ -12,7 +11,6 @@ class Solution:
                 if abs(prev - curr) <= k:
                     best = max(best, letters[prev])
 
-            # Append s[i] to the previous longest ideal subsequence
             letters[curr] = max(letters[curr], best + 1)
             res = max(res, letters[curr])
         return res
