092addb2e655298747349d2fc43a3af43fbd0378
dawitsema <128223243+dawitsema@users.noreply.github.com>
2024-05-17 16:16:43 +0300

    Time: 34 ms (73.67%) | Memory: 16.5 MB (26.72%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,13 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        memo = {}
-        def help(i):
-            if i == 1:
-                return 1
-            elif i == 0:
-                return 0
-            if i not in memo:
-                memo[i] = help(i-1) + help(i-2)
-            return memo[i]
+        dp = [0, 1]
+        for i in range(1, n + 1):
+            dp.append(dp[-1] + dp[-2])
         
-        return help(n)
+        return dp[n]
