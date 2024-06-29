fd316ef24e849c7554c02963c2362b1dde13fdae
dawitsema <128223243+dawitsema@users.noreply.github.com>
2024-06-29 20:33:28 +0300

    Time: 417 ms (99.85%) | Memory: 30.2 MB (97.59%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,48 +1,22 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        graph = defaultdict(list)
-        indegree = [0]*n
+        result = [set() for _ in range(n)]
+        indegrees = [0] * n
+        adj_list = collections.defaultdict(list)
         for u, v in edges:
-            graph[u].append(v)
-            indegree[v] += 1
-            
-        queue = deque([i for i, x in enumerate(indegree) if x == 0] )
-        ans = [set() for _ in range(n)]
-        
+            adj_list[u].append(v)
+            indegrees[v] += 1
+        queue = deque()
+        for i in range(len(indegrees)):
+            if not indegrees[i]:
+                queue.append(i)
         while queue:
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                for ng in graph[node]:
-
-                    ans[ng].add(node)
-                    for el in ans[node]:
-                        ans[ng].add(el)
-                    
-                    indegree[ng] -= 1
-                    if indegree[ng] == 0:
-                        queue.append(ng)
-        
-        ans = [sorted(list(gr)) for gr in ans]
-        return ans
-        # visited = set()
-        # temp = []
-
-        # def dfs(node):
-        #     visited.add(node)
-        #     for ng in graph[node]:
-        #         if ng not in visited:
-        #             dfs(ng)
-        #     temp.append(node)
-
-        # for i in range(n):
-        #     if i not in visited:
-        #         dfs(i)
-
-        # ans = [[] for _ in range(n)]
-        # for node in temp:
-        #     for ng in graph[node]:
-        #         ans[node].extend([ng] + ans[ng])
-        #     ans[node] = list(sorted(set(ans[node])))
-        
-        # return ans
-        
+            node = queue.popleft()
+            for neighbor in adj_list[node]:
+                result[neighbor].update(result[node], [node])
+                indegrees[neighbor] -= 1
+                if not indegrees[neighbor]:
+                    queue.append(neighbor)
+            result[node] = sorted(result[node])
+        return result
+#if you upvote this you will get a good news in 1 hour 
