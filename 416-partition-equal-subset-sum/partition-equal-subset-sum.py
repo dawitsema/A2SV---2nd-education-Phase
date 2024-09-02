3fe94bb14bc3ea38cb7556725b7d0243fae5f8fe
Dawit Sema <128223243+dawitsema@users.noreply.github.com>
2024-09-02 15:31:07 +0300

    Time: 2011 ms (12.87%) | Memory: 492.6 MB (5.17%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,18 +1,25 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums)%2:
-            return False
-        target = sum(nums)//2
+        summ = sum(nums)
+
+        if summ %2: return False
+
+        half = summ // 2
 
         memo = {}
-        def dp(i, val):
-            if val == 0: return True 
-            if val < 0 or i >= len(nums): return False 
 
-            if (i, val) not in memo:
-                memo[(i, val)] = dp(i+1, val-nums[i]) or dp(i+1, val)
-            
-            return memo[(i, val)]
+        def dp(idx , totsum):
 
-        return dp(0, target)
-        
+            if totsum == 0:
+                return True
+            if idx >= len(nums) :
+                return False
+
+            if (idx , totsum) not in memo:
+                memo[(idx , totsum )] = dp(idx + 1 , totsum - nums[idx]) or dp(idx + 1 , totsum)
+
+
+            return memo[idx , totsum]
+
+
+        return dp(0 , half)
