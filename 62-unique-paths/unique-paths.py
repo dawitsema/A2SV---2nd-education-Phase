7deba0aedf61f15cabd3db7a4ff973133b5c0fa5
Dawit Sema <128223243+dawitsema@users.noreply.github.com>
2024-10-18 18:46:03 +0300

    Time: 0 ms (100.00%) | Memory: 16.5 MB (81.79%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,15 +1,3 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
-        def help(i, j):
-            if i == m - 1 and j == n - 1:
-                return 1
-            if i > m - 1 or j > n - 1:
-                return 0
-            
-            if (i, j) not in memo:
-                memo[(i, j)] = help(i+1, j) + help(i, j+1)
-            
-            return memo[(i, j)]
-            
-        return help(0, 0)
+        return factorial(m + n - 2)//(factorial(m - 1)*factorial(n - 1))
