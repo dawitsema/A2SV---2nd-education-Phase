fa620b38702ae7e427182e82a19054b7e8a61c19
Dawit Sema <128223243+dawitsema@users.noreply.github.com>
2025-06-21 12:44:04 +0300

    Time: 51 ms (93.80%) | Memory: 18.2 MB (45.74%) - LeetSync

---
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        count = Counter(word)
+        res = len(word)
+
+        for a in count.values():
+            deleted = 0
+            for b in count.values():
+                if a > b:
+                    deleted += b
+                elif b > a + k:
+                    deleted += b - (a + k)
+            
+            res = min(res, deleted)
+        
+        return res
